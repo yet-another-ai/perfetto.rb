2481d02320d2234c3ffd558c41c7135ffb830fb9
Jamesits <osscontrib@ciexyz.net>
2025-07-10 15:27:49 +0800

    fix native extension compilation error on GCC 15

--- ext/perfetto/extconf.rb
+++ ext/perfetto/extconf.rb
@@ -5,7 +5,7 @@ require "mkmf"
 # Rubocop
 # rubocop:disable Style/GlobalVars
 $CXXFLAGS += " -std=c++17 -O3 -pthread"
-$CFLAGS += " -std=c11 -O3 -pthread"
 # rubocop:enable Style/GlobalVars
+append_cflags(["-std=c11", "-O3", "-pthread"])
 
 create_makefile "perfetto/perfetto_native"
